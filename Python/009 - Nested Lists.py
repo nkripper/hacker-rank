a4ce55f41dad021ef61892e25812541161dc6b8e
Nick Keeling <nkeeling@lsnetworks.net>
2021-10-18 09:17:33 -0700

    009-010 Completed successfully 011 - In Progress

--- Python/009 - Nested Lists.py
+++ Python/009 - Nested Lists.py
@@ -1,5 +1,23 @@
-# Not complete
 if __name__ == '__main__':
+    all_scores = []
+
     for _ in range(int(input())):
         name = input()
         score = float(input())
+        all_scores.append((name, score))
+
+    all_scores.sort(key=lambda x: x[1])
+
+    first_place = all_scores.pop(0)
+    second_place = []
+
+    for student in all_scores:
+        if student[1] != first_place[1]:
+            if len(second_place) == 0 or student[1] == second_place[0][1]:
+                second_place.append(student)
+            else:
+                break
+
+    second_place.sort()
+    for student in second_place:
+        print(student[0])
